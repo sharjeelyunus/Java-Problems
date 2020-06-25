7f1e27782e74f13507bcb9e80dafd77ef0fc897b
Sharjeel Yunus <61178058+sharjeelyunus@users.noreply.github.com>
2020-06-25 21:05:26 +0500

    Positive Negative Zero

---
+++ Positive Negative Zero/src/PositiveNegativeZero.java
@@ -0,0 +1,16 @@
+public class PositiveNegativeZero {
+    public static void main(String[] args) {
+        checkNumber(5);
+        checkNumber(-10);
+        checkNumber(0);
+    }
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("positive");
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else if (number == 0) {
+            System.out.println("zero");
+        }
+    }
+}
