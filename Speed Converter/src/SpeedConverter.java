6bfda40c20f02cf046b11e2c3c59a82a1d29e4ec
Sharjeel Yunus <61178058+sharjeelyunus@users.noreply.github.com>
2020-06-26 22:20:20 +0500

    👌 IMPROVE: Speed Converter

--- Speed Converter/src/SpeedConverter.java
+++ Speed Converter/src/SpeedConverter.java
@@ -5,4 +5,13 @@ public class SpeedConverter {
         }
         return Math.round(kilometersPerHour / 1.609);
     }
+
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+    }
 }
