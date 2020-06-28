360b42ddca4135c1812d612aa86aa686bcf56821
Sharjeel Yunus <61178058+sharjeelyunus@users.noreply.github.com>
2020-06-28 18:54:51 +0500

    Day of the week

---
+++ DayOfTheWeek/src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        printDayOfTheWeek(4);
+        printDayOfTheWeek(-1);
+
+    }
+
+    private static void printDayOfTheWeek(int day) {
+        switch (day) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            case 4:
+                System.out.println("Thursday");
+                break;
+
+            case 5:
+                System.out.println("Friday");
+                break;
+
+            case 6:
+                System.out.println("Saturday");
+                break;
+
+            default:
+                System.out.println("Invalid Day");
+        }
+    }
+}
