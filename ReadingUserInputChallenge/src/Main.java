c279f5bbc9edfb2b759cb00de89f0779d4e7348e
Sharjeel Yunus <61178058+sharjeelyunus@users.noreply.github.com>
2020-07-05 01:26:36 +0500

    🎉Reading User Input Challenge

---
+++ ReadingUserInputChallenge/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = 0;
+        int sum = 0;
+
+        while (true) {
+            int order = counter + 1;
+            System.out.println("Enter number #" + order + ":");
+
+            boolean isAnInt = scanner.hasNextInt();
+
+            if (isAnInt) {
+                int number = scanner.nextInt();
+                counter++;
+                sum += number;
+
+                if (counter == 10) {
+                    break;
+                }
+            } else {
+                System.out.println("Invalid Number");
+            }
+
+            scanner.nextLine();
+        }
+
+        System.out.println("Sum = " + sum);
+        scanner.close();
+    }
+}
