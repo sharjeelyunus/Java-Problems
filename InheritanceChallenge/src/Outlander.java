76aa2df7308f9d35784d7558db622e1f54b0e4a1
Sharjeel Yunus <61178058+sharjeelyunus@users.noreply.github.com>
2020-07-10 04:11:45 +0500

    🎉Inheritance Challenge

---
+++ InheritanceChallenge/src/Outlander.java
@@ -0,0 +1,8 @@
+public class Outlander extends Car {
+    private int roadServiceMonths;
+
+    public Outlander(int roadServiceMonths) {
+        super("Outlander", "4WD", 5, 5, 6, false);
+        this.roadServiceMonths = roadServiceMonths;
+    }
+}
