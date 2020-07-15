f7b5a94d500ce3763f8df72a0a742c4db4e29520
sharjeelyunus <sharjeel924@gmail.com>
2020-07-16 01:56:53 +0500

    🎉Encapsulation

---
+++ EncapsulationChallenge/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    Printer printer = new Printer(50,false);
+        System.out.println("Initial page count = " + printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(2);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
+    }
+}
